35f882f7afba218528060b9872e450fa35634f0f
Brian <bmalkowski@gmail.com>
2014-05-21 15:01:09 -0500

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get install -y vim git
-
+apt-get install -y vim git curl
 git clone https://github.com/gmarik/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
 curl https://raw.github.com/bmalkowski/dev/.vimrc > /home/vagrant/.vimrc
 vim +BundleInstall +qall 2&> /dev/null
